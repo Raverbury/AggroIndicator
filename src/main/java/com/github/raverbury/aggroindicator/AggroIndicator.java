03b358d9e8abef28dd62ab4a09f7ce6887379b7c
Raverbury <smurfjp198@gmail.com>
2023-06-24 15:13:45 +0700

    add configs

--- src/main/java/com/github/raverbury/aggroindicator/AggroIndicator.java
+++ src/main/java/com/github/raverbury/aggroindicator/AggroIndicator.java
@@ -1,32 +1,22 @@
 package com.github.raverbury.aggroindicator;
 
+import com.github.raverbury.aggroindicator.config.ClientConfig;
 import com.github.raverbury.aggroindicator.events.ClientEventHandler;
 import com.github.raverbury.aggroindicator.events.ServerEventHandler;
 import com.github.raverbury.aggroindicator.network.NetworkHandler;
 import com.mojang.logging.LogUtils;
 import io.netty.channel.Channel;
 import net.minecraft.client.Minecraft;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -58,6 +48,7 @@ public class AggroIndicator {
         ServerEventHandler.register();
         ClientEventHandler.register();
         NetworkHandler.register();
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ClientConfig.INSTANCE);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
