b2a56ae53b5fc393deb3d8799c7b223c6d18e653
Raverbury <smurfjp198@gmail.com>
2023-06-30 16:50:21 +0700

    fix crash on server startup, bump to 0.0.0.4

--- src/main/java/com/github/raverbury/aggroindicator/AggroIndicator.java
+++ src/main/java/com/github/raverbury/aggroindicator/AggroIndicator.java
@@ -5,13 +5,19 @@ import com.github.raverbury.aggroindicator.event.ClientEventHandler;
 import com.github.raverbury.aggroindicator.event.ServerEventHandler;
 import com.github.raverbury.aggroindicator.network.NetworkHandler;
 import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.RegisterEvent;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -37,12 +43,14 @@ public class AggroIndicator {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        ServerEventHandler.register();
-        ClientEventHandler.register();
-        NetworkHandler.register();
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ClientConfig.INSTANCE);
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> ClientEventHandler::register);
+        ServerEventHandler.register();
+        NetworkHandler.register();
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
     }
+
+
 }
