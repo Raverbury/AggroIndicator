7e1a7cd70bbe1b027020daabd241e44ffde872f8
Raverbury <smurfjp198@gmail.com>
2026-02-15 01:58:38 +0700

    - feat: add config for alert sound

--- common/src/main/java/io/github/raverbury/aggroindicator/ClientConfig.java
+++ common/src/main/java/io/github/raverbury/aggroindicator/ClientConfig.java
@@ -31,6 +31,7 @@ public class ClientConfig {
     private float alertIconSize = 30;
     private String alertColorHex = "0xFF6666";
     private int hideAlertAfterTicks = 0;
+    private boolean playAlertSound = false;
 
     public static ClientConfig cachedOrDefault() {
         if (CACHED_CONFIG != null) {
@@ -107,5 +108,9 @@ public class ClientConfig {
 
     public int getHideTimer() {
         return Math.max(0, hideAlertAfterTicks);
+    }
+
+    public boolean shouldPlayAlertSound(){
+        return playAlertSound;
     }
 }
