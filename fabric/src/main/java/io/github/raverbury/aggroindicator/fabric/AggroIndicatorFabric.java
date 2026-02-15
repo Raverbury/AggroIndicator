5a9aac1816ed19ed237291f79f02e62bde115438
Raverbury <smurfjp198@gmail.com>
2026-02-15 17:29:02 +0700

    - fix: call common init for fabric bruh

--- fabric/src/main/java/io/github/raverbury/aggroindicator/fabric/AggroIndicatorFabric.java
+++ fabric/src/main/java/io/github/raverbury/aggroindicator/fabric/AggroIndicatorFabric.java
@@ -1,6 +1,6 @@
 package io.github.raverbury.aggroindicator.fabric;
 
-import io.github.raverbury.aggroindicator.modules.AggroSoundPlayer;
+import io.github.raverbury.aggroindicator.CommonClass;
 import io.github.raverbury.aggroindicator.network.packets.S2CMobChangeTargetPacket;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
@@ -9,11 +9,7 @@ public class AggroIndicatorFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        // fabric on 1.21.1 needs to init even earlier, common just doesn't
-        // cut it
-        // todo: init is called twice, while for now empty, could be
-        //  problematic in the future if code is ever added
-        AggroSoundPlayer.init();
+        CommonClass.init();
         PayloadTypeRegistry.playS2C().register(S2CMobChangeTargetPacket.PACKET_TYPE, S2CMobChangeTargetPacket.CODEC);
     }
 }
